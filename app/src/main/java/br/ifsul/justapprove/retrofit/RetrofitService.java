a4a772408875fb3358097151648f869679ffd8ef
PenguinLx <julianol.schaeffer@gmail.com>
2024-12-04 09:04:38 -0300

    fix scroll in ranking and material(matheus did it all)

--- app/src/main/java/br/ifsul/justapprove/retrofit/RetrofitService.java
+++ app/src/main/java/br/ifsul/justapprove/retrofit/RetrofitService.java
@@ -20,7 +20,7 @@ public class RetrofitService {
 
     }
     private void initializeRetrofit() {
-        rfs = new Retrofit.Builder().baseUrl("http://192.168.1.8:8080").addConverterFactory(GsonConverterFactory.create(new Gson())).build(); /*pode ser a porta 9000 */
+        rfs = new Retrofit.Builder().baseUrl("http://172.16.10.184:8080").addConverterFactory(GsonConverterFactory.create(new Gson())).build(); /*pode ser a porta 9000 */
     }
 
     public Retrofit getRfs() {
